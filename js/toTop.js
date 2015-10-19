aa73dce7dc11bc458ce0e307a4de3de31749a0de
希望 <853090337@qq.com>
2015-10-19 15:23:07 +0800

    update

--- js/toTop.js
+++ js/toTop.js
@@ -1,5 +1,8 @@
 jQuery(document).ready(function($) {	
 			if($("meta[name=toTop]").attr("content")=="true"){
+				/*
+				 * 向body追加DOM节点
+				 */
 				$("<div id='toTop'><img src='img/totop.png'></div>").appendTo('body');
 				$("#toTop").css({
 					width: '50px',
@@ -14,7 +17,7 @@ jQuery(document).ready(function($) {
 						$("#toTop").hide();
 					}
 				$(window).scroll(function(event) {
-					/* Act on the event */
+					/*当前高度等于0时隐藏， 当前高度大于0时显示 */
 					if($(this).scrollTop()==0){
 						$("#toTop").hide();
 					}
